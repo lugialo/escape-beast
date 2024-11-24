ae62419b3d60bba0bac017d9240711b175fbdbb7
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-11-24 18:14:01 -0300

    Compilação

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1732482418,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/colisaoprotagonistajogavel-sheet0.png",
+		"images/colisaoprotagonistajogavel-sheet1.png",
+		"images/colisaocenariojogo.png",
+		"images/colisaoinimigo-sheet0.png",
+		"images/energiahud-sheet0.png",
+		"images/objetopagina-sheet0.png",
+		"images/colisaopedra.png",
+		"images/fundocenariograma.png",
+		"images/fundoobstaculos.png",
+		"images/arvores.png",
+		"images/colisaoinimigo2-sheet0.png",
+		"images/fundocenariopedra.png",
+		"images/imgpaginaestatica-sheet0.png",
+		"media/osttheme.ogg",
+		"media/chasetheme.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
